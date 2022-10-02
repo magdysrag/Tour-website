3c06d6f82a62ea3886b698e9e55a9a325b997480
magdy <magdysrag94@gmail.com>
2022-10-02 18:45:47 +0200

    finish search form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let links = document.querySelector(".links");
+let searchForm = document.querySelector(".search-form");
+let searchIcon = document.querySelector("#search-icon");
+let closeIcon = document.querySelector("#close-search");
 menu.addEventListener("click", appearIcon);
 function appearIcon() {
   links.classList.add("active");
@@ -7,4 +10,13 @@ cross.addEventListener("click", removeIcon);
 function removeIcon() {
   links.classList.remove("active");
 }
+console.log(searchForm);
 window.addEventListener("scroll", removeIcon);
+searchIcon.addEventListener("click", search);
+closeIcon.addEventListener("click", close);
+function search() {
+  searchForm.classList.add("active");
+}
+function close() {
+  searchForm.classList.remove("active");
+}
